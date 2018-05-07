8ac607e65cf4c9eca5e5988613e1c708432a212b
Justin <jkwok0714@gmail.com>
2018-05-06 22:17:13 -0700

    Handle dictionaries

--- server/database/database-serialize.js
+++ server/database/database-serialize.js
@@ -1,6 +1,9 @@
 const createSchema = (db) => {
   db.serialize(() => {
-    db.run('CREATE TABLE users (id integer PRIMARY KEY, username text UNIQUE, passcode text, image text)');
+    db.run('CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY, username text UNIQUE, passcode text, image text)');
+    db.run('CREATE TABLE IF NOT EXISTS hops (id integer PRIMARY KEY, userid number, name text, flavors text, description text)');
+    db.run('CREATE TABLE IF NOT EXISTS malts (id integer PRIMARY KEY, userid number, name text, flavors text, description text)');
+    db.run('CREATE TABLE IF NOT EXISTS yeast (id integer PRIMARY KEY, userid number, name text, flavors text, description text)');
   });
 };
 
---
+++ server/requestHandlers/dictionaryHandler.js
@@ -0,0 +1,51 @@
+const Helpers = require('../helpers.js');
+
+const handleAddEntry = (req, res, db) => {
+  console.log(req.body);
+  const body = req.body;
+  const userID = req.get('userID');
+  db.read(`INSERT INTO ${body.type}(userid, name, flavors, description) VALUES(?, ?, ?, ?)`,
+    [userID, body.name, body.flavors, body.description]).then(data => {
+    // const user = data[0];
+    res.status(200).send('Done');
+  }).catch(err => {
+    Helpers.log(err.message, 'R');
+    res.status(500).send(`Error occured: ${err.message}`);
+  });
+};
+
+const handleUpdateEntry = (req, res, db) => {
+  console.log(req.body);
+  const body = req.body;
+  db.read(`UPDATE ${body.type} SET name=?, flavors=?, description=? WHERE id=?`,
+    [body.name, body.flavors, body.description, body.id]).then(data => {
+    // const user = data[0];
+    res.status(200).send('Done');
+  }).catch(err => {
+    Helpers.log(err.message, 'R');
+    res.status(500).send(`Error occured: ${err.message}`);
+  });
+};
+
+const getUserDictionaries = (req, res, db) => {
+  let dataPackage = {};
+  const userID = req.get('userID');
+  db.read('SELECT * FROM hops WHERE userid=? OR userid=?', [userID, 0]).then(data => {
+    dataPackage.hops = data;
+    return db.read('SELECT * FROM malts WHERE userid=? OR userid=?', [userID, 0]);
+  }).then(data => {
+    dataPackage.malts = data;
+    return db.read('SELECT * FROM yeast WHERE userid=? OR userid=?', [userID, 0]);
+  }).then(data => {
+    dataPackage.yeast = data;
+    res.status(200).send(dataPackage);
+  }).catch(err => {
+    res.status(500).send(`Error occured: ${err.message}`);
+  })
+};
+
+module.exports = {
+  handleAddEntry,
+  handleUpdateEntry,
+  getUserDictionaries
+};
